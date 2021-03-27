158fca43e125979d67abfb3a440da36b7b4a29bb
vityaJN <akaniszviktor321@gmail.com>
2021-03-27 20:51:56 +0100

    test

--- fimilo-backend/src/main/java/backend/FimiloApplication.java
+++ fimilo-backend/src/main/java/backend/FimiloApplication.java
@@ -7,5 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FimiloApplication {
     public static void main(String[] args) {
         SpringApplication.run(FimiloApplication.class, args);
+
+
     }
 }
