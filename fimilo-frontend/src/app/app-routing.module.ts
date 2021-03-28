84c9c571d738f0d0550be7d119efcbf89b0b3344
theShakes07 <shakegame@citromail.hu>
2021-03-28 23:49:01 +0200

    FIM-2/Basic-routing

--- fimilo-frontend/src/app/app-routing.module.ts
+++ fimilo-frontend/src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: 'home',
+    loadChildren: () => import('./components/home/home.module').then(m => m.HomeModule)
+  },
+  {
+    path: '',
+    loadChildren: () => import('./components/home/home.module').then(m => m.HomeModule)
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: []
 })
 export class AppRoutingModule { }
