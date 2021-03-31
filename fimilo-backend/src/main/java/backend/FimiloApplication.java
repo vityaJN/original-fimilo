e5cd03e1b66f96fac7361833aac1586deb34c02c
vityaJN <akaniszviktor321@gmail.com>
2021-03-31 23:33:03 +0200

    FIM-9/backend/Pom-xml-fix

--- fimilo-backend/src/main/java/backend/FimiloApplication.java
+++ fimilo-backend/src/main/java/backend/FimiloApplication.java
@@ -2,8 +2,10 @@ package backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.fimilo")
+@EntityScan(basePackages = "com.fimilo")
 public class FimiloApplication {
     public static void main(String[] args) {
         SpringApplication.run(FimiloApplication.class, args);
