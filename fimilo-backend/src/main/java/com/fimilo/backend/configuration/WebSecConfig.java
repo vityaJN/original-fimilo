d5a4b75462f4e06238650b0f5c4bb004466e35d2
Kanisz Viktor <viktor.kanisz@ashszoftverhaz.hu>
2021-03-31 18:34:19 +0200

    FIM-9/backend/pom-xml-init-fix

---
+++ fimilo-backend/src/main/java/com/fimilo/backend/configuration/WebSecConfig.java
@@ -0,0 +1,22 @@
+package com.fimilo.backend.configuration;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+@EnableWebSecurity
+public class WebSecConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        CharacterEncodingFilter filter = new CharacterEncodingFilter();
+        http.addFilterBefore(filter, CsrfFilter.class)
+                .formLogin()
+                .loginPage("/login").defaultSuccessUrl("/", true).permitAll()
+                .and()
+                .authorizeRequests()
+                .anyRequest().authenticated();
+    }
+}
