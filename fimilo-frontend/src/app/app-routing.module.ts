8a4e9180cc0160276743ea3dd5b560d5568d65ff
theShakes07 <shakes07@citromail.hu>
2021-04-07 23:04:51 +0200

    Init

--- fimilo-frontend/src/app/app-routing.module.ts
+++ fimilo-frontend/src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+
 
 const routes: Routes = [
 
@@ -7,11 +9,7 @@ const routes: Routes = [
     path: 'home',
     loadChildren: () => import('./components/home/home.module').then(m => m.HomeModule)
   },
-  {
-    path: '',
-    loadChildren: () => import('./components/home/home.module').then(m => m.HomeModule)
-  },
-
+  { path: 'login', component: LoginComponent}
 ];
 
 @NgModule({
